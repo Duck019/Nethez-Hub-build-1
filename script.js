974cdc14b95d8b2e5a3311e56303792262232112
Duck019 <103079608+Duck019@users.noreply.github.com>
2024-11-04 07:14:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function redirecionarIA(ia) {
+
+    if (ia === 'z') {
+
+        window.location.href = "https://bots.easy-peasy.ai/bot/c9cfe34a-c27c-4716-bdc9-8ab447951301?_gl=1*mqfzd5*_gcl_au*MTE3NDU3MTYwLjE3MzA2NTAzNTg.";
+
+    } else if (ia === 'professorX') {
+
+        window.location.href = "https://bots.easy-peasy.ai/bot/b996302a-3e89-4caa-ab29-ba7b00f5a24e?_gl=1*us63kt*_gcl_au*MTU2ODk0Nzc2My4xNzMwNjc3NDky";
+
+    }
+
+}
